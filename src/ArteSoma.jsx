62f2c94c44b24c139881ae17e7868db752cd4413
shawnatavsky <shawnatavsky@gmail.com>
2026-03-23 20:31:48 -0600

    Update ArteSoma.jsx

--- src/ArteSoma.jsx
+++ src/ArteSoma.jsx
@@ -643,7 +643,7 @@ setTimeout(async () => {
   <div className="app">
 
       <div className="header">
-   
+    <p><br></p>
   <img src="/logo.png" alt="Arte Soma" className="header-logo" />
 
       <p className="header-byline">Micro-Practice Generator - v.01</p>
