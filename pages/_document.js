b69666089125aa80defa4e1225e45a49b44392e8
shawnatavsky <shawnatavsky@gmail.com>
2026-04-11 10:33:54 -0600

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,1 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
 
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.2/iframeResizer.contentWindow.min.js"></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
